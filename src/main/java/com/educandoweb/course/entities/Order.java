33cddde9a59d76a10e5aadd434c3f769130c3d40
Alan Franklin <alanfranklintx@gmail.com>
2023-03-10 23:46:48 -0300

    Subtotal & Total methods.

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -88,6 +88,15 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        double total = 0;
+        for (OrderItem x :
+                items) {
+           total += x.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
